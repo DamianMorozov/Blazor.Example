db97b62b1d5e2f3e6c0e940d3cd5e0945d388f1b
Damian Viktorovich Morozov <damian@morozov33.ru>
2020-11-05 17:30:02 +0300

    Added Dialog class and page

--- BlazorApp/Startup.cs
+++ BlazorApp/Startup.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BlazorApp.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BlazorApp.Data;
 
 namespace BlazorApp
 {
