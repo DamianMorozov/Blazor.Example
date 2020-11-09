01c1518fed59c97240172d2f84228763c47a6806
Damian Viktorovich Morozov <damian@morozov33.ru>
2020-11-09 17:21:58 +0300

    Changed BlazorSignalRApp

--- BlazorSignalRApp/Server/Controllers/WeatherForecastController.cs
+++ BlazorSignalRApp/Server/Controllers/WeatherForecastController.cs
@@ -1,10 +1,9 @@
 ﻿using BlazorSignalRApp.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace BlazorSignalRApp.Server.Controllers
 {
